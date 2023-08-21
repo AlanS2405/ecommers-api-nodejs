f5d7ac2298accdb948adc8d2d6eecdf26c0df4e9
AlanS2405 <alan.dario.solis@gmail.com>
2023-08-21 15:47:13 -0300

    tests de users hasta put pasados

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,6 +4,7 @@ require('../models')
 
 const URL_BASE = '/api/v1/users'
 let TOKEN
+let userId
 
 beforeAll( async () => {
     const user = {
@@ -18,7 +19,7 @@ beforeAll( async () => {
         TOKEN = res.body.token
 })
 
-test("GET -> 'baseUrl', should return send status 200, res.body id defined, res.body.firstName === user.name and res.body.length === 1",
+test("GET -> 'URL_BASE', should return send status 200, res.body id defined, res.body.firstName === user.name and res.body.length === 1",
     async () => {
         const res = await request(app)
         .get(URL_BASE)
@@ -30,3 +31,39 @@ test("GET -> 'baseUrl', should return send status 200, res.body id defined, res.
         expect(res.body).toBeDefined()
         expect(res.body).toHaveLength(1)
     });
+
+test("POST -> 'URL_BASE', should return send status 201, res.body is defined and res.body.firstName === user.firstName", async () => {
+
+    const user = {
+        firstName:"Alan",
+        lastName:"Solis",
+        email:"alan.solis@mail.com",
+        password:"alan12345",
+        phone:"+5491123456789"
+    }
+
+    const res = await request(app)
+    .post(URL_BASE)
+    .send(user)
+
+    userId = res.body.id
+
+    expect(res.status).toBe(201)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(user.firstName)
+});
+
+test("PUT -> 'URL_BASE/:id', should return status 200, res.body is defined and res.body.firstName === user.firstName", async () => {
+    const user = {
+        firstName:"Alan Dario",
+    }
+
+    const res = await request(app)
+    .put(`${URL_BASE}/${userId}`)
+    .send(user)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(user.firstName)
+})
