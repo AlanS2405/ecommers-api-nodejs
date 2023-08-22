e931ef741dd69f1b0b8fa71423daa27e6b0b35f8
AlanS2405 <alan.dario.solis@gmail.com>
2023-08-22 19:32:59 -0300

    controllers get y create de cart editados

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
 const User = require("./User");
@@ -5,3 +6,10 @@ const User = require("./User");
 //En la tabla product creamos la columna categoryId
 Product.belongsTo(Category)//categoryId
 Category.hasMany(Product)
+
+//En la tabla Cart creamos las columnas userId y productId
+Cart.belongsTo(User) //userId
+User.hasMany(Cart)
+
+Cart.belongsTo(Product) //productId
+Product.hasMany(Cart)
